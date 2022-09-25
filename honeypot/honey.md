b644d4d5e2697940f17f1efec42f3f2f0dbc2999
xfaraday <theonlyfaraday@gmail.com>
2022-09-25 00:36:06 -0700

    add docs and setup script

---
+++ honeypot/honey.md
@@ -0,0 +1,10 @@
+# Cyber Threat Intelligence
+## Subproject Honeypot
+
+The files listed in this directory are capable of spinning up a preconfigured honeypot with HoneyTrap![https://github.com/honeytrap/honeytrap] and an ELK stack.
+
+Files:
+- config.toml | Config file for HoneyTrap
+- docker-compose.yml | Docker environment with ELK stack
+- notes | txt file with sequential commands needed for setup
+- setup-env.sh | Bash script that sets up the honeypot automatically
--- honeypot/setup-env.sh
+++ honeypot/setup-env.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+#root check
+if [ "$EUID" -ne 0 ]; then
+  echo 'Error: Please run as root'
+  exit 1
+fi
+
+echo -e "\nPlease stop this script if internet access is not avaliable...in"
+sleep 1
+echo "5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
+
+#getting docker
+which docker || snap install docker
+
+mkdir honepot; cd honeypot
+
+#create docker network
+docker network create honeytrap
+
+#increasing docker limit size
+sysctl -w vm.max_map_count=262144
+
+#get config files
+wget https://raw.githubusercontent.com/xFaraday/CyberNewCareers/main/honeypot/config.toml || curl https://raw.githubusercontent.com/xFaraday/CyberNewCareers/main/honeypot/config.toml -o config.toml
+wget https://raw.githubusercontent.com/xFaraday/CyberNewCareers/main/honeypot/docker-compose.yml || curl https://raw.githubusercontent.com/xFaraday/CyberNewCareers/main/honeypot/docker-compose.yml -o docker-compose.yml
+
+#elastic search startup
 mkdir -p ./data/elasticsearch/data && sudo chown -R 1000:1000 ./data/elasticsearch
 
-docker-compose -f docker-compose-honeytrap-playground.yml up
+#start up containers
+docker-compose -f docker-compose.yml up -d
